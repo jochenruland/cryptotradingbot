6aeaf7e2058fd989a997714d0859142bdb1d0d74
jochenruland <jochenhgruland@gmail.com>
2022-01-11 18:28:53 +0100

    EDIT filename of contract to be deployed

--- migrations/2_TradingbotDeploy.js
+++ migrations/2_TradingbotDeploy.js
@@ -1,6 +1,6 @@
-const Tradingbot = artifacts.require("Tradingbot");
+const Tradingbot_v2 = artifacts.require("Tradingbot_v2");
 
 // Do not forget to pass constructor arguments to deploy function!
 module.exports = function (deployer) {
-  deployer.deploy(Tradingbot);
+  deployer.deploy(Tradingbot_v2);
 };
