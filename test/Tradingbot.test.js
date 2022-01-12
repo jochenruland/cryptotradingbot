e72c199ebbab9b8a599dd95291ec9d1ec7d1fa97
jochenruland <jochenhgruland@gmail.com>
2022-01-12 11:31:24 +0100

    ADD test cases

--- test/Tradingbot.test.js
+++ test/Tradingbot.test.js
@@ -1,13 +1,12 @@
-
-//------------------------------------------------------------------------------------------
-
+const { expectRevert, time } = require('@openzeppelin/test-helpers');
 const assert = require('assert');
-
 const Web3 = require('web3'); // Web3 Library
 
-const web3 = new Web3(provider); // web3 instance using the current provider
+const web3provider = new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/e9fbb5967c14460cbf78edd9d129532f');
 
-const compiledContractJson = require('../build/contracts/Tradingbot_v2.json');
+const web3 = new Web3(web3provider);
+
+const compiledContractJson = require('../build/contracts/Tradingbot.json');
 
 let accounts;
 let contractInstance;
@@ -21,18 +20,29 @@ beforeEach(async () => {
 
 
 describe('Testing Tradingbot_v2', () => {
-  it('Deploys a contract', () => {
+  it('Contract available on Rinkeby', () => {
     console.log(contractInstance.options.address);
+    console.log(accounts);
     assert.ok(contractInstance.options.address);
   });
 
-  /*
   it('Initializes owner by msg.sender', async () => {
     const owner = await contractInstance.methods.owner().call();
     console.log(accounts[0], owner);
     assert.equal(accounts[0], owner);
   });
 
+  it('Initializes contribution period and minAmount', async () => {
+    await contractInstance.methods.initialize(100, 120).send({
+      from: accounts[0],
+      gas: '1000000'
+    })
+    const min = contractInstance.methods.minAmount().call();
+    assert.equal(100, min);
+  });
+
+
+  /*
   it('Can recieve Ether', async () => {
     const tx = await contractInstance.methods.deposit().send({
       from: accounts[0],
