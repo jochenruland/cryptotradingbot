13b97a6baa7f77306078367a629a4c7f7febe172
jochenruland <jochenhgruland@gmail.com>
2022-01-12 09:50:25 +0100

    ADD compiled artifacts

--- migrations/2_TradingbotDeploy.js
+++
@@ -1,6 +0,0 @@
-const Tradingbot_v2 = artifacts.require("Tradingbot_v2");
-
-// Do not forget to pass constructor arguments to deploy function!
-module.exports = function (deployer) {
-  deployer.deploy(Tradingbot_v2);
-};
