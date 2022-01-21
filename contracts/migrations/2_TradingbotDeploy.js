916e2f01047c5362cdaab9847db0c97012f27e7b
jochenruland <jochenhgruland@gmail.com>
2022-01-21 12:19:37 +0100

    DELETE artifacts for new deployment

--- contracts/migrations/2_TradingbotDeploy.js
+++
@@ -1,6 +0,0 @@
-const Tradingbot = artifacts.require("Tradingbot");
-
-// Do not forget to pass constructor arguments to deploy function!
-module.exports = function (deployer) {
-  deployer.deploy(Tradingbot);
-};
